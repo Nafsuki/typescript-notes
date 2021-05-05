63894f8ac132d68663fc8247915db799211a4317
natsuki yamaguchi <natsuki.yamaguchi.62@gmail.com>
2021-05-05 20:11:32 +0200

    revised

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Data from './data.json';
+import TestComponent from './TestComponent';
 
+// json data *important!
+type USERS = typeof Data;
+
+
+//
 const name = 'natsuki';
 
 let nameChange = 'natsuki';
@@ -80,12 +87,106 @@ let key: keyof KEYS;
 key = 'primary';
 
 // typeof + keyof
+const SPORTS = {
+  soccor: 'Soccor',
+  baseball: 'Baseball',
+};
 
-function App() {
+let keySports: keyof typeof SPORTS;
+keySports = 'soccor';
+
+// enum
+enum OS {
+  Windows,
+  Mac,
+  Linux,
+}
+
+interface PC {
+  id: number;
+  OSType: OS;
+}
+
+const PC1: PC = {
+  id: 1,
+  OSType: OS.Windows,
+}
+
+const PC2: PC = {
+  id: 2,
+  OSType: OS.Linux,
+}
+
+//型の互換性
+const comp1 = 'test';
+let comp2: string = comp1;
+
+let comp3: string = 'test';
+// let comp4: 'test' = comp3; // 出来ない
+
+let funcComp1 = (x: number) => {};
+let funcComp2 = (x: string) => {};
+// funcComp1 = funcComp2; // 出来ない
+// funcComp2 = funcComp1; // 出来ない
+
+//Generics *important
+interface GEN<T> {
+  item: T;
+}
+
+const gen0: GEN<string> = {
+  item: 'hello'
+}
+// const gen1: GEN = { // 出来ない
+//   item: 'hello'
+// }
+const gen2: GEN<number> = {
+  item: 1,
+}
+
+interface GEN1<T = string> {
+  item: T;
+}
+const gen3: GEN1 = {
+  item: 'hello',    //出来る
+}
+
+interface GEN2<T extends string | number> {
+  item: T;
+}
+const gen4: GEN2<number> = {
+  item: 3,
+}
+
+function funcGen<T>(props: T) {
+  return {item: props}
+}
+const gen5 = funcGen('test');
+const gen6 = funcGen<string | null>(null);
+
+function funcGen1<T extends string | null>(props: T) {
+  return {value: props};
+} 
+const gen7 = funcGen1('hello');
+const gen8 = funcGen1<string>('hello');
+
+interface Props {
+  price: number;
+}
+function funcGen3<T extends Props>(props: T){
+  return {value: props.price}
+}
+const gen9 = funcGen3({price: 10});
+
+const funcGen4 = <T extends Props>(props: T) => {
+  return {value: props.price};
+}
+
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <TestComponent text='Hello Natsuki!' />
       </header>
     </div>
   );
